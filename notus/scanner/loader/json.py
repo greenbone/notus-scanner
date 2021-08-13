af08c3146788ec2cb4239a4fa33018a944125acc
Björn Ricks <bjoern.ricks@greenbone.net>
2021-08-13 12:28:28 +0200

    Implement a advisory loader from JSON format
    In contrast to the CSV format a single JSON file only contains
    advisories for one operating system version. Also the structure of the
    JSON format corresponds to the models already.

---
+++ notus/scanner/loader/json.py
@@ -0,0 +1,141 @@
+# Copyright (C) 2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import logging
+
+from datetime import datetime, timezone
+from json.decoder import JSONDecodeError
+from pathlib import Path
+
+from ..errors import AdvisoriesLoadingError
+from ..models.advisory import (
+    Advisory,
+    PackageAdvisories,
+    Severity,
+)
+from ..models.package import parse_rpm_package
+
+from .loader import AdvisoriesLoader
+
+logger = logging.getLogger(__name__)
+
+
+def _get_operating_system_file_name(operating_system: str) -> str:
+    return operating_system.strip().replace(' ', '_')
+
+
+class JSONAdvisoriesLoader(AdvisoriesLoader):
+    def __init__(self, advisories_directory_path: Path):
+        self._advisories_directory_path = advisories_directory_path
+
+    def load(self, operating_system: str) -> PackageAdvisories:
+        os_file_name = _get_operating_system_file_name(operating_system)
+        json_file_path = (
+            self._advisories_directory_path / f"{os_file_name}.notus"
+        )
+        if not json_file_path.exists():
+            raise AdvisoriesLoadingError(
+                f'Could not load advisories from {json_file_path.absolute()}. '
+                'File does not exist.'
+            )
+
+        package_advisories = PackageAdvisories()
+        if json_file_path.stat().st_size < 2:
+            # the minimim size of a json file is 2 bytes ({} or [])
+            return package_advisories
+
+        with json_file_path.open('r', encoding="UTF-8") as f:
+            try:
+                json_data = json.load(f)
+            except JSONDecodeError as e:
+                raise AdvisoriesLoadingError(
+                    f'Could not load advisories from '
+                    f'{json_file_path.absolute()}. Error in line {e.lineno} '
+                    f'while decoding JSON data.'
+                ) from None
+
+        for advisory_data in json_data.get("advisories", []):
+            print(advisory_data)
+            if not "oid" in advisory_data:
+                logger.error('No OID found for JSON advisory %s', advisory_data)
+                continue
+
+            try:
+                # parse required data
+                oid = advisory_data['oid']
+                title = advisory_data['title']
+                creation_date = datetime.fromtimestamp(
+                    advisory_data['creation_date'], timezone.utc
+                )
+                last_modification = datetime.fromtimestamp(
+                    advisory_data['last_modification'], timezone.utc
+                )
+                advisory_id = advisory_data['advisory_id']
+                advisory_xref = advisory_data['advisory_xref']
+                fixed_packages = advisory_data['fixed_packages']
+                severity_data = advisory_data['severity']
+                severity_origin = severity_data['origin']
+                severity_date = datetime.fromtimestamp(
+                    severity_data['date'], tz=timezone.utc
+                )
+            except (KeyError, TypeError) as e:
+                logger.warning(
+                    'Error while parsing %s from %s. Error was %s',
+                    advisory_data,
+                    str(json_file_path.absolute()),
+                    e,
+                )
+                continue
+
+            # parse optional data
+            summary = advisory_data.get('summary')
+            insight = advisory_data.get('insight')
+            affected = advisory_data.get('affected')
+            impact = advisory_data.get('impact')
+            xrefs = advisory_data.get('xrefs', [])
+            cves = advisory_data.get('cves', [])
+            cvss_v2 = severity_data.get('cvss_v2')
+            cvss_v3 = severity_data.get('cvss_v3')
+
+            severity = Severity(
+                origin=severity_origin,
+                date=severity_date,
+                cvss_v2=cvss_v2,
+                cvss_v3=cvss_v3,
+            )
+            advisory = Advisory(
+                oid=oid,
+                title=title,
+                creation_date=creation_date,
+                last_modification=last_modification,
+                advisory_id=advisory_id,
+                advisory_xref=advisory_xref,
+                severity=severity,
+                summary=summary,
+                insight=insight,
+                affected=affected,
+                impact=impact,
+                xrefs=xrefs,
+                cves=cves,
+            )
+
+            for package_name in fixed_packages:
+                package = parse_rpm_package(package_name)
+                package_advisories.add_advisory_for_package(package, advisory)
+
+        return package_advisories
---
+++ tests/loader/test_json.py
@@ -0,0 +1,116 @@
+# Copyright (C) 2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from datetime import datetime, timezone
+from pathlib import Path
+from unittest import TestCase
+
+from notus.scanner.errors import AdvisoriesLoadingError
+from notus.scanner.loader.json import JSONAdvisoriesLoader
+from notus.scanner.models.package import parse_rpm_package
+
+_here = Path(__file__).parent
+
+
+class JSONAdvisoriesLoaderTestCase(TestCase):
+    def test_unknown_file(self):
+        loader = JSONAdvisoriesLoader(advisories_directory_path=_here)
+
+        with self.assertRaises(AdvisoriesLoadingError):
+            loader.load('foo')
+
+    def test_empty_file(self):
+        loader = JSONAdvisoriesLoader(advisories_directory_path=_here)
+
+        advisories = loader.load('EmptyOS')
+        self.assertEqual(len(advisories), 0)
+
+    def test_example(self):
+        loader = JSONAdvisoriesLoader(advisories_directory_path=_here)
+
+        advisories = loader.load('EulerOS V2.0SP1')
+
+        self.assertIsNotNone(advisories)
+        self.assertEqual(len(advisories), 55)
+
+        package1 = parse_rpm_package('openssh-6.6.1p1-25.4.h3.x86_64')
+        package2 = parse_rpm_package('openssh-clients-6.6.1p1-25.4.h3.x86_64')
+
+        package_advisories1 = advisories.get_package_advisories_for_package(
+            package1
+        )
+        package_advisories2 = advisories.get_package_advisories_for_package(
+            package2
+        )
+
+        self.assertEqual(len(package_advisories1), 1)
+        self.assertEqual(len(package_advisories2), 1)
+
+        # get first PackageAdvisory from the sets
+        package_advisory1 = next(iter(package_advisories1))
+        package_advisory2 = next(iter(package_advisories2))
+
+        self.assertEqual(package_advisory1.advisory, package_advisory2.advisory)
+
+        advisory = package_advisory1.advisory
+
+        self.assertEqual(advisory.oid, '1.3.6.1.4.1.25623.1.1.2.2016.1008')
+        self.assertEqual(
+            advisory.title,
+            # pylint: disable=line-too-long
+            'Huawei EulerOS: Security Advisory for openssh (EulerOS-SA-2016-1008)',
+        )
+        self.assertEqual(advisory.advisory_id, 'EulerOS-SA-2016-1008')
+        self.assertEqual(
+            advisory.advisory_xref,
+            # pylint: disable=line-too-long
+            'https://developer.huaweicloud.com/ict/en/site-euleros/euleros/security-advisories/EulerOS-SA-2016-1008',
+        )
+        self.assertEqual(advisory.cves, ['CVE-2016-1908', 'CVE-2016-3115'])
+        self.assertEqual(advisory.severity.origin, 'NVD')
+        self.assertEqual(
+            advisory.severity.cvss_v2, 'AV:N/AC:L/Au:N/C:P/I:P/A:P'
+        )
+        self.assertEqual(
+            advisory.severity.cvss_v3,
+            'CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H',
+        )
+        self.assertEqual(
+            advisory.summary,
+            # pylint: disable=line-too-long
+            "The remote host is missing an update for the Huawei EulerOS 'openssh' package(s) announced via the EulerOS-SA-2016-1008 advisory.",
+        )
+        self.assertEqual(
+            advisory.insight,
+            # pylint: disable=line-too-long
+            'It was discovered that the OpenSSH server did not sanitize data received in requests to enable X11 forwarding. An authenticated client with restricted SSH access could possibly use this flaw to bypass intended restrictions. (CVE-2016-3115)\n\n'
+            # pylint: disable=line-too-long
+            'An access flaw was discovered in OpenSSH, the OpenSSH client did not correctly handle failures to generate authentication cookies for untrusted X11 forwarding. A malicious or compromised remote X application could possibly use this flaw to establish a trusted connection to the local X server, even if only untrusted X11 forwarding was requested. (CVE-2016-1908)',
+        )
+        self.assertIsNone(advisory.impact)
+        self.assertEqual(
+            advisory.creation_date,
+            datetime(2021, 5, 27, 7, 3, 13, tzinfo=timezone.utc),
+        )
+        self.assertEqual(
+            advisory.last_modification,
+            datetime(2021, 7, 22, 2, 24, 2, tzinfo=timezone.utc),
+        )
+        self.assertEqual(
+            advisory.severity.date,
+            datetime(2018, 9, 11, 10, 29, tzinfo=timezone.utc),
+        )
