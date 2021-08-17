54e763faa24d8266dd3a97689b3f8a9e6d697b5c
Björn Ricks <bjoern.ricks@greenbone.net>
2021-08-17 10:24:59 +0200

    Update Publisher class to be an Abstract Base Class
    Improve the Publisher class to better understand it's meaning.

--- notus/scanner/messaging/publisher.py
+++ notus/scanner/messaging/publisher.py
@@ -15,9 +15,18 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from abc import ABC, abstractmethod
+
 from ..messages.message import Message
 
 
-class Publisher:
+class Publisher(ABC):
+    """An Abstract Base Class (ABC) for publishing Messages
+
+    When updating to Python > 3.7 this should be converted into a
+    typing.Protocol
+    """
+
+    @abstractmethod
     def publish(self, message: Message) -> None:
         raise NotImplementedError()
