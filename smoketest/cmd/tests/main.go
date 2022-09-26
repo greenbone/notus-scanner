46dfe5ecd13bf3d5f69e90b4ca0e2e41d5332c85
Philipp Eder <philipp.eder@greenbone.net>
2022-09-26 12:35:40 +0200

    Add: smoke test for slackware packages (#284)
    Add: smoke test for slackware packages
    
    Notus smoketests are very simple test cases to verify the core functionality of Notus.
    
    It is not meant to be fine granular tests but rather if the overall flow:
    ```
    ospd-openvas -> openvas -> gatherpackagelist.nasl -> notus
    ```
    
    is functioning.
    
    Currently this is tested for
    
    - Slackware 15
    
    To add new images as a target you have to
    
    - create a new target in the Makefile to spwan the new host
    - create a new target to delete the new host
    - adapt hosts.txt with the hostname username and password in the form us: `user:pass@host` in a new line
    
    There are some requirements for a test image:
    
    1. the image must have advisories and products defined in `/var/lib/notus` for the vulnerabilities of the image
    1. the image must start an ssh server with a known username and password to login

---
+++ smoketest/cmd/tests/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/greenbone/notus-scanner/smoketest/products"
+	"github.com/greenbone/ospd-openvas/smoketest/connection"
+	"github.com/greenbone/ospd-openvas/smoketest/nasl"
+	"github.com/greenbone/ospd-openvas/smoketest/policies"
+)
+
+
+func ParseHosts(hostPath string) ([]string, error) {
+	result := make([]string, 0)
+	f, err := os.Open(hostPath)
+	if err != nil {
+		return nil, err
+	}
+	fs := bufio.NewScanner(f)
+	fs.Split(bufio.ScanLines)
+	for fs.Scan() {
+		line := strings.TrimSpace(fs.Text())
+		// ignore comments or emppty lines
+		if len(line) > 0 && !strings.HasPrefix(line, "#") {
+			result = append(result, line)
+		}
+	}
+
+	return result, nil
+}
+
+func main() {
+	vtDIR := flag.String("vt-dir", "/var/lib/openvas/plugins", "A path to existing plugins.")
+	policyPath := flag.String("policy-path", "/usr/local/src/policies", "path to policies.")
+	ospdSocket := flag.String("u", "/run/ospd/ospd-openvas.sock", "path the ospd unix socket")
+	productPath := flag.String("product-path", "/var/lib/notus/products", "path to the notus product definitions")
+	hostsTxt := flag.String("hosts-txt", "/usr/local/src/notus/hosts.txt", "path to the hosts definition file.")
+	flag.Parse()
+	rc := 0
+	naslCache, err := nasl.InitCache(*vtDIR)
+	if err != nil {
+		panic(err.Error())
+	}
+	policyCache, err := policies.InitCache(*policyPath)
+	if err != nil {
+		panic(err.Error())
+	}
+	lm, err := products.CreateLookupMap(*productPath)
+	if err != nil {
+		panic(err.Error())
+	}
+	hosts, err := ParseHosts(*hostsTxt)
+	if err != nil {
+		panic(err.Error())
+	}
+	protocoll := "unix"
+	address := *ospdSocket
+	co := connection.New(protocoll, address, "", "", false)
+	gplt := products.GatherPackageListTests{
+		NASLCache:     naslCache,
+		PolicyCache:   policyCache,
+		ProductLookup: lm,
+		Sender:        co,
+	}
+	for _, host := range hosts {
+		if hr := gplt.TestHost(host); hr.Success {
+			fmt.Printf("success\n")
+		} else {
+			fmt.Printf("failed: %s\n", hr.Description)
+			rc += 1
+		}
+	}
+	os.Exit(rc)
+}
