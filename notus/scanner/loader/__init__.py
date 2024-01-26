b4a076ee1baf5fcc54c490c2325eae8f25a7441f
Björn Ricks <bjoern.ricks@greenbone.net>
2024-01-26 12:35:13 +0100

    Change: Update copyright header for 2024

--- notus/scanner/loader/__init__.py
+++ notus/scanner/loader/__init__.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: 2021-2023 Greenbone AG
+# SPDX-FileCopyrightText: 2021-2024 Greenbone AG
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from .loader import AdvisoriesLoader
 from .json import JSONAdvisoriesLoader
+from .loader import AdvisoriesLoader
--- notus/scanner/messaging/__init__.py
+++ notus/scanner/messaging/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: 2021-2023 Greenbone AG
+# SPDX-FileCopyrightText: 2021-2024 Greenbone AG
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
